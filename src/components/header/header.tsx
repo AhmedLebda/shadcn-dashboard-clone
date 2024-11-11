d88db92511c198bb486abdb13af1f705ef9466f3
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-11-11 21:47:37 +0200

    add: dark mode

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "../theme/mode-toggle"
 import MainNav from "./main-nav/main-nav"
 import Search from "./search/search"
 import TeamSwitcher from "./team-switcher/team-switcher"
@@ -5,12 +6,13 @@ import UserNav from "./user-nav/user-nav"
 
 const Header = () => {
     return (
-        <header className='border-b flex py-4 items-center px-4 flex-wrap gap-4'>
+        <header className='z-10 bg-primary-foreground flex py-4 items-center px-4 flex-wrap gap-4 sticky top-0'>
             <TeamSwitcher />
             <MainNav />
             <div className='md:ml-auto flex items-center space-x-4'>
                 <Search />
                 <UserNav />
+                <ModeToggle />
             </div>
         </header>
     )
